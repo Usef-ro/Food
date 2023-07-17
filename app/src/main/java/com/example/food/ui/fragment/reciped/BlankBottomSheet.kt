cd8c4a883485c90361600b5da61f2f263dd88fc7
usef <jo.joy31@yahoo.com>
2023-07-17 11:05:47 -0700

    fix bug chipGroupe

--- app/src/main/java/com/example/food/ui/fragment/reciped/BlankBottomSheet.kt
+++ app/src/main/java/com/example/food/ui/fragment/reciped/BlankBottomSheet.kt
@@ -1,20 +1,41 @@
 package com.example.food.ui.fragment.reciped
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import com.example.food.R
+import com.example.food.databinding.FragmentBlankBottomSheetBinding
+import com.example.food.databinding.FragmentRecipedBinding
+import com.example.food.util.constants
+import com.example.food.viewModel.recipesViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import java.lang.Exception
+import java.util.Locale
 
 class BlankBottomSheet : BottomSheetDialogFragment() {
 
+    lateinit var recipesViewModel: recipesViewModel
+
+    var mealType=constants.DEFAULT_MEAL_TYPE
+    var mealTypeId=0
+    var dietType=constants.DEFAULT_DIET_TYPE
+    var dietTypeId=0
+
+    private var _binding: FragmentBlankBottomSheetBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        recipesViewModel=ViewModelProvider(requireActivity()).get(recipesViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -22,7 +43,47 @@ class BlankBottomSheet : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank_bottom_sheet, container, false)
+        _binding= FragmentBlankBottomSheetBinding.inflate(inflater, container, false)
+        val view = binding.root
+        recipesViewModel.readMealAndDietType.asLiveData().observe(viewLifecycleOwner,{value->
+            mealType=value.selectMealsType
+            dietType=value.selectDietType
+            updateChip(value.selectMealsTypeId, _binding!!.maelTypeChip)
+            updateChip(value.selectDieTypeId, _binding!!.chipGroupeDiet)
+        })
+    binding.maelTypeChip.setOnCheckedChangeListener { group, checkedIds ->
+        val chip=group.findViewById<Chip>(checkedIds)
+        val selectMealType=chip.text.toString().toLowerCase(Locale.ROOT)
+        mealType=selectMealType
+        mealTypeId=checkedIds
+    }
+
+        binding.chipGroupeDiet.setOnCheckedChangeListener { group, checkedIds ->
+            val chip=group.findViewById<Chip>(checkedIds)
+            val selectDietType=chip.text.toString().toLowerCase(Locale.ROOT)
+            dietType=selectDietType
+            dietTypeId=checkedIds
+        }
+        binding.buttonSubmit.setOnClickListener {
+recipesViewModel.saveMealAndDietType(
+    mealType,
+    mealTypeId,
+    dietType,
+    dietTypeId
+)
+        }
+
+    return view
+    }
+
+    private fun updateChip(selectDieTypeId: Int, chipGroupeDiet: ChipGroup) {
+        if(selectDieTypeId!=0){
+            try{
+                chipGroupeDiet.findViewById<Chip>(selectDieTypeId).isChecked=true
+            }catch (e:Exception){
+                Log.e("updateChip",""+e.message.toString())
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/food/viewModel/recipesViewModel.kt
+++ app/src/main/java/com/example/food/viewModel/recipesViewModel.kt
@@ -2,6 +2,8 @@ package com.example.food.viewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.food.domain.database.DataStoreRepository
 import com.example.food.util.constants
 import com.example.food.util.constants.DEFAULT_DIET_TYPE
 import com.example.food.util.constants.DEFAULT_MEAL_NUMBER
@@ -11,17 +13,38 @@ import com.example.food.util.constants.QUERY_DIET
 import com.example.food.util.constants.QUERY_INFORMATION
 import com.example.food.util.constants.QUERY_NUMBER
 import com.example.food.util.constants.QUERY_TYPE
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-
-class recipesViewModel(app:Application):AndroidViewModel(app)
+@HiltViewModel
+class recipesViewModel  @Inject constructor(app:Application,val dataStoreRepository: DataStoreRepository    ):AndroidViewModel(app)
 {
+
+    private var mealType= DEFAULT_MEAL_TYPE
+    private var dietType= DEFAULT_DIET_TYPE
+
+    val readMealAndDietType=dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType:String,mealTypedId:Int,diet:String,dietId:Int)=
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAndDietType(mealType,mealTypedId,diet,dietId)
+        }
     fun applyQueries():HashMap<String,String>{
         val query= HashMap<String, String>()
 
+        viewModelScope.launch {
+            readMealAndDietType.collect{value->
+                mealType=value.selectMealsType
+                dietType=value.selectDietType
+            }
+        }
+
         query[QUERY_NUMBER]=DEFAULT_MEAL_NUMBER
         query[QUERY_TYPE]= constants.API_KEY
-        query[QUERY_DIET]=DEFAULT_MEAL_TYPE
-        query[QUERY_API]=DEFAULT_DIET_TYPE
+        query[QUERY_DIET]=mealType
+        query[QUERY_API]=dietType
         query[QUERY_INFORMATION]="true"
 
         return query
